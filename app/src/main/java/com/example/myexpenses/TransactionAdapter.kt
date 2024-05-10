1b47c48c57c154eeca7ae48f9db89bb3377db487
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-05-10 23:59:48 +0530

    Update TransactionAdapter.kt

--- app/src/main/java/com/example/myexpenses/TransactionAdapter.kt
+++ app/src/main/java/com/example/myexpenses/TransactionAdapter.kt
@@ -11,7 +11,21 @@ import androidx.recyclerview.widget.RecyclerView
 class TransactionAdapter(private var transactions: List<Transaction>) :
     RecyclerView.Adapter<TransactionAdapter.TransactionHolder>() {
 
-    class Transactio
+    class TransactionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val label : TextView = view.findViewById(R.id.label)
+        val amount : TextView = view.findViewById(R.id.amount)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.transaction_layout, parent, false)
+        return TransactionHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TransactionHolder, position: Int) {
+        val transaction = transactions[position]
+        val context = holder.amount.context
+
+        if(transaction.amount >= 0){
             holder.amount.text = "+ $%.2f".format(transaction.amount)
             holder.amount.setTextColor(ContextCompat.getColor(context, R.color.green))
         }else {
