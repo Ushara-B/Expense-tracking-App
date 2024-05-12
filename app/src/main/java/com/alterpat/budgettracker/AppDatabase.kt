8f407bf414ea890928ade748375f4348d4e1c1bb
Ushara Wijesinghe <127342854+Ushara-B@users.noreply.github.com>
2024-05-12 12:47:29 +0530

    Appdatabase ,main and transaction error fix

--- app/src/main/java/com/alterpat/budgettracker/AppDatabase.kt
+++ app/src/main/java/com/alterpat/budgettracker/AppDatabase.kt
@@ -3,6 +3,7 @@ package com.alterpat.budgettracker
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entiti
+@Database(entities = arrayOf(Transaction::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao() : TransactionDao
 }
--- app/src/main/java/com/alterpat/budgettracker/TransactionAdapter.kt
+++ app/src/main/java/com/alterpat/budgettracker/TransactionAdapter.kt
@@ -21,6 +21,16 @@ class TransactionAdapter(private var transactions: List<Transaction>) :
         return TransactionHolder(view)
     }
 
+    override fun onBindViewHolder(holder: TransactionHolder, position: Int) {
+        val transaction = transactions[position]
+        val context = holder.amount.context
+
+        if(transaction.amount >= 0){
+            holder.amount.text = "+ $%.2f".format(transaction.amount)
+            holder.amount.setTextColor(ContextCompat.getColor(context, R.color.green))
+        }else {
+            holder.amount.text = "- $%.2f".format(Math.abs(transaction.amount))
+            holder.amount.setTextColor(ContextCompat.getColor(context, R.color.red))
         }
 
         holder.label.text = transaction.label
